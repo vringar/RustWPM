eea4eb8d0421bba206f43b7ef12bc3ce080320f0
vringar <h_dieter@mail.ru>
2020-04-16 21:03:08 +0200

    Writing my first types

---
+++ README.md
@@ -0,0 +1,13 @@
+# RustWPM
+
+This is a toy project attempting to rewrite [OpenWPM](https://github.com/mozilla/OpenWPM) 
+in Rust to get a better understanding of both rustlang and OpenWPM.
+
+## First Steps
+
+Reimplement the basic structure of
+1. TaskManager
+2. Browser
+3. DataAggregator
+
+However I want to use websockets instead of unix sockets.
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,21 @@ use webdriver_client::firefox::GeckoDriver;
 use webdriver_client::messages::NewSessionCmd;
 use webdriver_client::Driver;
 use std::{thread, time};
+use log::debug;
+use simple_logger;
+use crate::CommandSequenceBuilder;
+
 
 fn main() {
-    println!("Hello, world!");
+    simple_logger::init().unwrap();
+    debug!("First log");
+    test();
     let driver = GeckoDriver::spawn().unwrap();
     let session = driver.session(& NewSessionCmd::default()).unwrap();
     session.go("https://example.com").unwrap();
     thread::sleep(time::Duration::from_secs(10));
+}
+
+fn test() {
+    let builder = CommandSequenceBuilder{};
 }
