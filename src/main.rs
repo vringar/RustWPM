5600787f9558a27ae841385b2a37eb12bb546730
vringar <h_dieter@mail.ru>
2020-04-17 00:30:34 +0200

    Starting to split stuff out

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
-use webdriver_client::firefox::GeckoDriver;
-use webdriver_client::messages::NewSessionCmd;
-use webdriver_client::Driver;
-use std::{thread, time};
+
 use log::debug;
 use simple_logger;
-use crate::CommandSequenceBuilder;
+use webdriver_client::Error;
 
+mod commands;
+use commands::*;
 
-fn main() {
+mod browser;
+
+fn main() -> Result<(), Error> {
     simple_logger::init().unwrap();
     debug!("First log");
-    test();
-    let driver = GeckoDriver::spawn().unwrap();
-    let session = driver.session(& NewSessionCmd::default()).unwrap();
-    session.go("https://example.com").unwrap();
-    thread::sleep(time::Duration::from_secs(10));
+    let sequence = test().unwrap();
+    let browser = browser::Browser::new(sequence);
+    browser.execute_command_sequence()?;
+    Ok(())
 }
 
-fn test() {
-    let builder = CommandSequenceBuilder{};
+fn test() -> Result<CommandSequence,()> {
+    let mut builder = CommandSequenceBuilder::new();
+    let command = Commands::Get("http://asd.com".to_owned());
+    builder.append_command(command)?;
+    let cs = builder.build();
+    Ok(cs)
 }
