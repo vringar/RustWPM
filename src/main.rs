b236a988eae043f44de05513f3b70448513709ce
vringar <h_dieter@mail.ru>
2020-08-09 18:39:51 +0200

    Introducing eyre and tracing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 
-use log::debug;
-use simple_logger;
-use webdriver_client::Error;
-
+use tracing::debug;
+use tracing_subscriber;
 mod commands;
 use commands::*;
 
 mod browser;
+use browser::Browser;
 
-fn main() -> Result<(), Error> {
-    simple_logger::init().unwrap();
+use eyre::Result;
+
+fn main() -> Result<()> {
+
+    tracing_subscriber::fmt().init();
     debug!("First log");
     let sequence = test().unwrap();
-    let browser = browser::Browser::new(sequence);
+    let browser = Browser::new(sequence);
     browser.execute_command_sequence()?;
     Ok(())
 }
 
-fn test() -> Result<CommandSequence,()> {
+fn test() -> Result<CommandSequence> {
     let mut builder = CommandSequenceBuilder::new();
-    let command = Commands::Get("http://asd.com".to_owned());
+    let command = Commands::Get("http://example.com".to_owned());
     builder.append_command(command)?;
     let cs = builder.build();
     Ok(cs)
